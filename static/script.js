8d729d2fa6e467290f3ac431174a72a064ecdd3d
gbalite <gbalite@uw.edu>
2024-12-16 12:41:40 -0800

    added functionality to nav bar and added project pages (no content yet)

--- static/script.js
+++ static/script.js
@@ -1,1 +1,14 @@
 
+
+// CARD NAVIGATION
+document.getElementById("moonlight").addEventListener("click", () => {
+    window.location.href = "moonlight.html";
+});
+
+document.getElementById("focus-dashboard").addEventListener("click", () => {
+    window.location.href = "focus.html";
+});
+
+document.getElementById("project-loast").addEventListener("click", () => {
+    window.location.href = "loast.html";
+});
